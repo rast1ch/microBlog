ba109d267d4e85cc7ee9d7f9abe89b7fc8063550
rast1ch <rast1ch2286@gmail.com>
2021-02-03 12:23:08 +0200

    Involved both delete and update functions

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ app_name = 'blog'
 urlpatterns  = [
     path("", views.HomePageView.as_view(), name = 'index'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'post_detail'),
-    path('post/new/', views.BlogCreateView.as_view(), name = 'new')
+    path('post/<int:pk>/edit' , views.BlogUpdateView.as_view(), name = 'post_edit'),
+    path('post/<int:pk>/delete' , views.BlogDeleteView.as_view() , name = 'post_delete'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'new'),
+
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Post
 # Create your views here.
@@ -10,6 +11,7 @@ class HomePageView(generic.ListView):
     model = Post
     template_name = 'blog/index.html'
 
+
 class BlogDetailView(generic.DetailView):
     model  = Post
     template_name = 'blog/pos_detail.html'
@@ -19,3 +21,15 @@ class BlogCreateView(generic.CreateView):
     template_name = 'blog/post_new.html'
     model = Post
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(generic.UpdateView):
+    template_name = 'blog/post_edit.html'
+    model = Post
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(generic.DeleteView):
+    template_name = 'blog/post_delete.html'
+    model = Post
+    success_url = reverse_lazy('blog:index')

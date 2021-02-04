5e97524f537139b7e96957651e0d5c114c0ea687
rast1ch <rast1ch2286@gmail.com>
2021-02-04 15:28:41 +0200

    Implemented account functions(Creating, logging in, logging out)

--- microBlog/settings.py
+++ microBlog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'blog:index'
+LOGOUT_REDIRECT_URL = 'blog:index'
